66138c1c54a3daee6ce5f1bdb316592bee44297c
Dante Camarena <mhdcj@hotmail.com>
2017-04-11 13:46:44 -0400

    4.3 Quadratic Transformations

--- Assets/Scripts/MeshlessShapeMatching.cs
+++ Assets/Scripts/MeshlessShapeMatching.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using System.Linq;
 using MathNet.Numerics;
 using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Single;
 
 public class MeshlessShapeMatching : MonoBehaviour, IDeformer
 {
@@ -17,7 +16,7 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
     Vector<float>[] x;
     float[] m;
     Vector<float>[] q;
-    Vector<float>[] p;
+    IEnumerable<Matrix<float>> p;
     Vector3[] f;
     Vector<float> x_0_cm;
     Matrix<float> A_qq;
@@ -31,6 +30,13 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
     public LayerMask collidesWith;
     public float pullForce = 1;
     MeshCollider meshCollider;
+    Vector<float>[] q_hat;
+    Matrix<float> A_hat_qq;
+    public bool quadratic;
+    Matrix<float> A_pq;
+    Matrix<float> A_hat_pq;
+    Matrix<float>[] q_rm;
+    Matrix<float>[] q_hat_rm;
 
     // Use this for initialization
     void Start()
@@ -73,7 +79,18 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
         q = x_0.Select(x0_i=>x0_i - x_0_cm).ToArray();
         A_qq = q.Zip(m, (q_i, m_i) => m_i * q_i.ToColumnMatrix() * q_i.ToRowMatrix()).Aggregate((sum,mat)=>sum+mat).Inverse();
 
-        
+
+        q_hat = q.Select(q_i => Vector<float>.Build.Dense(new[] { q_i[0], q_i[1], q_i[2], q_i[0] * q_i[0], q_i[1] * q_i[1], q_i[2] * q_i[2], q_i[0] * q_i[1], q_i[1] * q_i[2], q_i[2] * q_i[0] })).ToArray();
+        q_rm = q.Select((q_i, i) => q_i.ToRowMatrix() * m[i]).ToArray();
+        q_hat_rm = q_hat.Select((q_i, i) => q_i.ToRowMatrix() * m[i]).ToArray();
+
+        var A_hat_qq_list = q_hat.Zip(m, (q_hat_i, m_i) => m_i * q_hat_i.ToColumnMatrix() * q_hat_i.ToRowMatrix()).ToArray();
+        var A_hat_qq_sum = A_hat_qq_list.Aggregate((sum, mat) => sum + mat);
+        //Todo: why is this a pseudoinverse?
+        A_hat_qq = A_hat_qq_sum.PseudoInverse();
+
+        A_pq = Matrix<float>.Build.Dense(3, 3, 0);
+        A_hat_pq = Matrix<float>.Build.Dense(3, 9, 0);
     }
 
     void Update()
@@ -88,26 +105,43 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
         for (int i = 0; i < particles.Length; i++)  x[i].SetValue(particles[i].position);
 
         var x_cm = x.Mean(m);
-        p = x.Select(x_i => x_i - x_cm).ToArray();
-        var A_pq = Matrix<float>.Build.Dense(3, 3, 0);
+        
 
-        //Todo:could cache weighted q_i row matrices
-        for (int i = 0; i < q.Length; i++)
+        
+        A_pq.Clear();
+        foreach (var mat in x.Zip(q_rm, (x_i, q_rm_i) => (x_i - x_cm).ToColumnMatrix() * q_rm_i))
         {
-            A_pq += m[i] * p[i].ToColumnMatrix() * q[i].ToRowMatrix();
+            A_pq.Add(mat, A_pq);
         }
+        if(quadratic)
+        {
+            A_hat_pq.Clear();
+            foreach (var mat in x.Zip(q_hat_rm, (x_i, q_hat_rm_i) => (x_i - x_cm).ToColumnMatrix() * q_hat_rm_i))
+            {
+                A_hat_pq.Add(mat, A_hat_pq);
+            }
+        } 
 
         var S = (A_pq.Transpose() * A_pq).Sqrt();
         var R = A_pq * S.Inverse();
-        var A = A_pq * A_qq;
-        var detA = A.Determinant();
-        A /= Mathf.Pow(detA, 1 / 3f);
-        print(A.Determinant() +" : "+detA);
-        var T = beta * A + (1 - beta) * R;
 
+        Vector3[] g;
+        if (!quadratic)
+        {
+            var A = A_pq * A_qq;
+            var detA = A.Determinant();
+            A /= Mathf.Pow(detA, 1 / 3f);
+            var T = beta * A + (1 - beta) * R;
+            g = q.Select(q_i => (T * q_i) + x_cm).Select(v => v.ToUnity()).ToArray();
+        }
+        else
+        {
+            var A_hat = A_hat_pq * A_hat_qq;
+            var R_hat = Matrix<float>.Build.Dense(3, 9, (i, j) => j < 3 ? R[i, j] : 0);
+            var T_hat = beta * A_hat + (1 - beta) * R_hat;
+            g = q_hat.Select(q_hat_i => (T_hat * q_hat_i) + x_cm).Select(v => v.ToUnity()).ToArray();
 
-
-        var g = q.Select(q_i => (T * q_i) + x_cm).Select(v=>v.ToUnity()).ToArray();
+        }
 
         var h = Time.fixedDeltaTime;
 
@@ -128,12 +162,13 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
             particles[i].velocity = start + middle + end;
             if (Vector3.Dot(particles[i].velocity, start) < 0) particles[i].velocity *= bounceVelDamp;
             particles[i].velocity *= velocityDamp;
+            
+            var hitInfos = Physics.RaycastAll(new Ray(particles[i].position - particles[i].velocity*h, particles[i].velocity.normalized),
+                particles[i].velocity.magnitude*h*2, collidesWith ).Where(hit=>hit.transform!=this.transform);
 
-            RaycastHit hitInfo;
-            bool didhit = Physics.Raycast(new Ray(particles[i].position - particles[i].velocity*h, particles[i].velocity.normalized),
-                out hitInfo, particles[i].velocity.magnitude*h*2, collidesWith );
+            bool didhit = hitInfos.Any();
 
-            particles[i].position = didhit? hitInfo.point : particles[i].position + particles[i].velocity * h;
+            particles[i].position = didhit? hitInfos.First().point : particles[i].position + particles[i].velocity * h;
             particles[i].velocity = didhit? Vector3.zero : particles[i].velocity;
         }
 
@@ -142,7 +177,7 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
         var targetQuat = R.ToUnity().ExtractRotation();
         transform.rotation = Quaternion.Slerp(transform.rotation, targetQuat, alpha);
 
-        currentMesh.SetVertices(particles.Select(p => transform.InverseTransformPoint(p.position)).ToList());
+        currentMesh.SetVertices(particles.Select(part => transform.InverseTransformPoint(part.position)).ToList());
         currentMesh.RecalculateNormals();
         meshCollider.sharedMesh = currentMesh;
     }
@@ -150,13 +185,13 @@ public class MeshlessShapeMatching : MonoBehaviour, IDeformer
 
     public Vector3[] WorldVertices
     {
-        get { return particles.Select(p => p.position).ToArray(); }
+        get { return particles.Select(part => part.position).ToArray(); }
     }
 
     public void SetWorldVertexPosition(int index, Vector3 position, bool resetVelocity = false)
     {
         //particles[index].position = position;
-        targets.Add(index,position);
+        targets[index] = position;
         if (resetVelocity) particles[index].velocity = Vector3.zero;
  
     }
