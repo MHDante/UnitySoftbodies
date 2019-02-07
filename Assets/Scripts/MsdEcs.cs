e80539743ec01a5d495d3736f831fd7f03053a53
MHDante <mhdcj@hotmail.com>
2019-02-07 03:01:54 -0500

    Added Comments, options and tooltips to code.

--- Assets/Scripts/MsdEcs.cs
+++ Assets/Scripts/MsdEcs.cs
@@ -1,211 +1,201 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Mathematics.Extensions;
 using Unity.Mathematics;
 using UnityEngine;
 
+// ReSharper disable InconsistentNaming - Some Variables are named according to their name in [Muller05]'s paper
+
+/// <summary>
+/// This component is an implementation of the 2005 paper
+///
+/// [Muller05]
+/// Muller, Matthias, et al. "Meshless deformations based on shape matching."
+/// ACM transactions on graphics (TOG). Vol. 24. No. 3. ACM, 2005.
+/// https://www.cs.drexel.edu/~david/Classes/Papers/MeshlessDeformations_SIG05.pdf
+///
+/// It also uses a computational approach to find rotations described in:
+/// [Muller16]
+/// Muller, Matthias, et al. "A robust method to extract the rotational part of deformations."
+/// Proceedings of the 9th International Conference on Motion in Games. ACM, 2016
+/// https://animation.rwth-aachen.de/media/papers/2016-MIG-StableRotation.pdf
+///  
+/// </summary>
+[RequireComponent(typeof(MeshFilter))]
 public class MsdEcs : MonoBehaviour
 {
-
-   
-    [Range(0, 1)]
+    [Range(0, 1), Tooltip(
+         "This determines the acceleration with which the object tries to deform back into its original shape. " +
+         "A value of 1 results in rigid-like behaviour.")]
     public float Alpha = 1;
 
-    [Range(0, 1)]
-    public float Beta = 1;
+    [Range(0, 1), Tooltip(
+         "Interpolation between 0: rotation only or 1: shearing/bending. Works only on Linear and Quadratic Modes ")]
+    public float Beta = 0;
 
-    public float Drag;
 
+    [Tooltip("Dampening of velocity. This is applied to all particle rigidbodies")]
+    public float Drag = 0;
+    private float oldDrag;
 
+    [Tooltip("The matching mode determines how the shape can deform. All modes allow small deformations, but this controls changes in the overall frame of the shape." +
+             "Rigid allows only rotations. Linear allows only shears and stretches. Quadratic allows bending. ")]
+    public MatchingModes MatchingMode = MatchingModes.Quadratic;
+
+    [Tooltip("If true, this transform will update it's position and rotation to the center of mass of the particles. A bit slow.")]
+    public bool ShouldUpdatePivot = true;
+
+    // Object properties
+    private MeshFilter meshFilter;
     private Quaternion initialRotation;
-    private Vector3[] vertices;
-    private float9[] q_tilde;
+    private Transform particleParent;
+
+    // collections to avoid re-allocating. Variables are named according to their name in [Muller05]'s paper<
     private float3[] q;
     private float3[] x;
     private float[] m;
-    private MeshFilter meshFilter;
-    private Rigidbody[] bodies;
-    private float3 x0Cm;
-    private quaternion rq = quaternion.identity;
+    private float3 x0cm;
     private float3x3 Aqq;
+    private float3x3 T;
+    //Quadratic terms
+    private float9[] q_tilde;
     private float9x9 Aqq_tilde;
-    private Transform parent;
+    private float3x9 T_tilde;
+    private float3x9 R_tilde;
+
+    // Rendering and physic entities;
+    private Particle[] particles;
+    private Vector3[] vertices;
+
+    // Seed for ExtractRotation. We start with the identity quaternion because the particles' original position (q)
+    // calculated using world-aligned axes. See [Muller2016]
+    // Rq stands for "R quaternion", implying this is the quaternion version of R.
+    private quaternion Rq = quaternion.identity;
+
 
     public void Awake()
     {
         initialRotation = transform.rotation;
-        //var oldCollider = GetComponent<Collider>();
+
         meshFilter = GetComponent<MeshFilter>();
-
         vertices = meshFilter.mesh.vertices;
-        bodies = new Rigidbody[vertices.Length];
-        var colliders = new List<Collider>();
+        particles = CreateParticlesAtVertices();
 
-        parent = new GameObject("Particles").transform;
-        parent.SetParent(transform, false);
-
-        for (var i = 0; i < vertices.Length; i++)
-        {
-            var o = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            o.name = $"Particle {i}";
-            o.transform.SetParent(parent, false);
-            o.transform.localPosition = vertices[i];
-            var c = o.GetComponent<SphereCollider>();
-            foreach (var cc in colliders)
-            {
-                Physics.IgnoreCollision(c, cc, true);
-            }
-            colliders.Add(c);
-            //c.radius = 0.1f;
-            o.transform.localScale = Vector3.one * 0.1f;
-            var rb = o.AddComponent<Rigidbody>();
-            rb.velocity = Vector3.zero;
-            rb.freezeRotation = true;
-            rb.useGravity = true;
-            rb.isKinematic = false;
-            rb.drag = Drag;
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
-            bodies[i] = rb;
-        }
-
-        x = new float3[bodies.Length];
-        m = new float[bodies.Length];
-        GetPositionsAndMasses(bodies, x, m);
-        x0Cm = CenterOfMass(x, m);
-
-        q = x.Select(x0i => x0i - x0Cm).ToArray();
-        q_tilde = q.Select(qi => new float9(
-            qi.x, qi.y, qi.z,
-            qi.x * qi.x, qi.y * qi.y, qi.z * qi.z,
-            qi.x * qi.y, qi.y * qi.z, qi.z * qi.x)
-        ).ToArray();
-
+        // Initial allocations
+        x = new float3[particles.Length];
+        m = new float[particles.Length];
+        q = new float3[particles.Length];
+        q_tilde = new float9[particles.Length];
         Aqq = float3x3.zero;
         Aqq_tilde = float9x9.zero;
+
+        GetPositionsAndMasses(particles, x, m, out x0cm);
+
+        // We calculate the quadratic terms and Aqq matrices regardless of the mode in case it gets switched at runtime.
         for (int i = 0; i < x.Length; i++)
         {
-            var mass = bodies[i].mass;
-            Aqq += mass * mathExt.mulT(q[i], q[i]);
-            Aqq_tilde += mass * mathExt.mulT(q_tilde[i], q_tilde[i]);
+            q[i] = x[i] - x0cm;
+            q_tilde[i] = GetQuadratic(q[i]);
+            Aqq += m[i] * mathExt.mulT(q[i], q[i]);
+            Aqq_tilde += m[i] * mathExt.mulT(q_tilde[i], q_tilde[i]);
         }
 
         Aqq = math.inverse(Aqq);
         Aqq_tilde = mathExt.inverse(Aqq_tilde);
     }
 
+
+
+    // The contents of this method are outlined in [Muller05]
     public void FixedUpdate()
     {
+        var mode = MatchingMode;
 
-        GetPositionsAndMasses(bodies, x, m);
-        var xcm = CenterOfMass(x, m);
+        GetPositionsAndMasses(particles, x, m, out var xcm);
 
         float3x3 Apq = float3x3.zero;
         float3x9 Apq_tilde = float3x9.zero;
 
         for (int i = 0; i < x.Length; i++)
         {
-            var mass = m[i];
             var pi = x[i] - xcm;
-            Apq += mass * mathExt.mulT(pi, q[i]);
-            Apq_tilde += mass * mathExt.mulT(pi, q_tilde[i]);
+            Apq += m[i] * mathExt.mulT(pi, q[i]);
+
+            if (MatchingMode == MatchingModes.Quadratic)
+                Apq_tilde += m[i] * mathExt.mulT(pi, q_tilde[i]);
         }
 
-        ExtractRotation(ref Apq, ref rq);
 
-        
-        var t = transform;
-        parent.SetParent(null, true);
-        t.position = xcm;
-        t.rotation = initialRotation * rq;
-        parent.SetParent(t, true);
+        // This part is different. It doesn't use polar decomposition. It instead uses the method outlined in [Muller16]
+        ExtractRotation(ref Apq, ref Rq);
+        var R = new float3x3(Rq);
 
+        if (ShouldUpdatePivot) UpdatePivot(xcm, Rq);
 
-        var R = new float3x3(rq);
-        
-        var R_tilde = float3x9.zero;
-        R_tilde.c0 = R.c0;
-        R_tilde.c1 = R.c1;
-        R_tilde.c2 = R.c2;
+        switch (mode)
+        {
+            case MatchingModes.Linear:
+            {
+                var A = math.mul(Apq, Aqq);
+            
+                // Divide by cube root of determinant to preserve volume
+                var det = math.determinant(A);
+                var cbrtDet = math.pow(math.abs(det), 1 / 3.0f);
+                var div = det < 0 ? -cbrtDet : cbrtDet;
+                A = A / div;
 
-        var A = math.mul(Apq, Aqq);
-        PerserveVolume(ref A);
+                T = Beta * A + (1 - Beta) * R;
+                break;
+            }
+            case MatchingModes.Quadratic:
+            {
+                R_tilde.c0 = R.c0;
+                R_tilde.c1 = R.c1;
+                R_tilde.c2 = R.c2;
 
-        var A_tilde = mathExt.mul(Apq_tilde, Aqq_tilde);
+                var A_tilde = mathExt.mul(Apq_tilde, Aqq_tilde);
 
-        var T = Beta * A + (1 - Beta) * R;
-        var T_tilde = Beta * A_tilde + (1 - Beta) * R_tilde;
+                // Todo: Can we preserve volume on a 3x9 transformation?
 
-        DrawPt(xcm);
+                T_tilde = Beta * A_tilde + (1 - Beta) * R_tilde;
+                break;
+            }
+        }
+
         for (int i = 0; i < x.Length; i++)
         {
-            var rot1 = math.mul(T, q[i]);
-            var rot2 = mathExt.mul(T_tilde,  q_tilde[i]);
-            var gi = rot2 + xcm;
-            var diff = gi - x[i];
+            float3 gi;
+            switch (mode)
+            {
+                case MatchingModes.Rigid:
+                    gi = math.mul(R, q[i]) + xcm; 
+                    break;
+                case MatchingModes.Linear: 
+                    gi = math.mul(T, q[i]) + xcm; 
+                    break;
+                case MatchingModes.Quadratic: 
+                    gi = mathExt.mul(T_tilde, q_tilde[i]) + xcm; 
+                    break;
+                default: 
+                    throw new ArgumentOutOfRangeException();
+            }
 
-
-            var accel = Alpha / Time.fixedDeltaTime * diff;
-            bodies[i].AddForce(accel, ForceMode.VelocityChange);
+            var accel = (Alpha / Time.fixedDeltaTime) * (gi - x[i]);
+            particles[i].Rigidbody.AddForce(accel, ForceMode.VelocityChange);
         }
     }
 
-    void DrawPt(Vector3 gi, Color? color = null)
+    private void UpdatePivot(in float3 xcm, in quaternion rotDif)
     {
-        ;
-        var c = color ?? Color.white;
-        Debug.DrawLine(gi, gi + new Vector3(0, 0, .1f), c);
-        Debug.DrawLine(gi, gi + new Vector3(0, .1f, 0), c);
-        Debug.DrawLine(gi, gi + new Vector3(.1f, 0, 0), c);
-
-    }
-    private void Update()
-    {
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] = transform.InverseTransformPoint(bodies[i].position);
-        }
-        meshFilter.mesh.vertices = vertices;
-        meshFilter.mesh.RecalculateNormals();
-        meshFilter.mesh.RecalculateBounds();
+        var t = transform;
+        particleParent.SetParent(null, true);
+        t.position = xcm;
+        t.rotation = initialRotation * rotDif;
+        particleParent.SetParent(t, true);
     }
 
-
-
-    private void GetPositionsAndMasses(Rigidbody[] rs, float3[] ps, float[] ms)
-    {
-        var dt = Time.fixedDeltaTime;
-        for (int i = 0; i < rs.Length; i++)
-        {
-            var position = rs[i].position;
-            ps[i] = position + rs[i].velocity * dt;
-            ms[i] = rs[i].mass;
-        }
-    }
-
-    public static float3 CenterOfMass(float3[] ps, float[] ms)
-    {
-        float3 r = float3.zero;
-        float m = 0;
-        for (var i = 0; i < ps.Length; i++)
-        {
-            r += ps[i] * ms[i];
-            m += ms[i];
-        }
-        return r / m;
-    }
-
-
-    public float3 Sum(float[] value, float[] weight = null)
-    {
-        float3 r = 0;
-        for (var i = 0; i < value.Length; i++)
-        {
-            r += weight?[i] * value[i] ?? value[i];
-        }
-        return r;
-    }
-
-    // From: https://animation.rwth-aachen.de/media/papers/2016-MIG-StableRotation.pdf
+    // [Muller16]
     void ExtractRotation(ref float3x3 a, ref quaternion r, int maxIterations = 9)
     {
         for (var i = 0; i < maxIterations; i++)
@@ -220,18 +210,123 @@ public class MsdEcs : MonoBehaviour
         }
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static float9 GetQuadratic(float3 qi) => new float9(
+        qi.x, qi.y, qi.z,
+        qi.x * qi.x, qi.y * qi.y, qi.z * qi.z,
+        qi.x * qi.y, qi.y * qi.z, qi.z * qi.x);
 
-    public void PerserveVolume(ref float3x3 a)
+
+    private void Update()
     {
-        var det = math.determinant(a);
-        var cbrtDet = math.pow(math.abs(det), 1 / 3.0);
-        var div = det < 0 ? -cbrtDet : cbrtDet;
-        a = a / (float)div;
+        foreach (var particle in particles)
+        {
+            for (var i = 0; i < particle.Indexes.Length; i++)
+            {
+                var index = particle.Indexes[i];
+                Vector3 offset = particle.Offsets[i];
+                vertices[index] = transform.InverseTransformPoint(particle.Rigidbody.position) + offset;
+            }
+        }
+        meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+        meshFilter.mesh.RecalculateBounds();
+
+        if (Drag != oldDrag)
+        {
+            oldDrag = Drag;
+            foreach (var body in particles)
+            {
+                body.Rigidbody.drag = Drag;
+            }
+        }
+    }
+
+    private void GetPositionsAndMasses(Particle[] rs, float3[] ps, float[] ms, out float3 centerOfMass)
+    {
+
+        float3 weightedSum = float3.zero;
+        float totalMass = 0;
+
+        var dt = Time.fixedDeltaTime;
+        for (int i = 0; i < rs.Length; i++)
+        {
+            var position = rs[i].Rigidbody.position;
+            ps[i] = position + rs[i].Rigidbody.velocity * dt;
+            ms[i] = rs[i].Rigidbody.mass;
+
+            weightedSum += ps[i] * ms[i];
+            totalMass += ms[i];
+        }
+
+        centerOfMass = weightedSum / totalMass;
+    }
+
+    private Particle[] CreateParticlesAtVertices()
+    {
+
+        var result = new Particle[vertices.Length];
+        var colliders = new List<Collider>();
+
+        particleParent = new GameObject("Particles").transform;
+        particleParent.SetParent(transform, false);
+
+        for (var i = 0; i < result.Length; i++)
+        {
+            var rb = MakeParticle(vertices[i], colliders);
+            result[i].Rigidbody = rb;
+            result[i].Indexes = new[] { i };
+            result[i].Offsets = new[] { float3.zero };
+        }
+
+        return result;
+    }
+
+    private Rigidbody MakeParticle(Vector3 localPosition, List<Collider> colliders)
+    {
+        var o = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        o.name = "Particle";
+        o.transform.SetParent(particleParent, false);
+        o.transform.localPosition = localPosition;
+        var c = o.GetComponent<SphereCollider>();
+        foreach (var cc in colliders)
+        {
+            Physics.IgnoreCollision(c, cc, true);
+        }
+
+        colliders.Add(c);
+        //c.radius = 0.1f;
+        o.transform.localScale = Vector3.one * 0.1f;
+        var rb = o.AddComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.drag = Drag;
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        return rb;
     }
 
 
+    // ReSharper disable once UnusedMember.Local - Useful for debugging.
+    void DrawPt(Vector3 gi, Color? color = null)
+    {
+        var c = color ?? Color.white;
+        Debug.DrawLine(gi, gi + new Vector3(0, 0, .1f), c);
+        Debug.DrawLine(gi, gi + new Vector3(0, .1f, 0), c);
+        Debug.DrawLine(gi, gi + new Vector3(.1f, 0, 0), c);
+
+    }
 }
 
+public enum MatchingModes
+{
+    Rigid,
+    Linear,
+    Quadratic
+}
 
-
+public struct Particle
+{
+    public int[] Indexes;
+    public float3[] Offsets;
+    public Rigidbody Rigidbody;
+}
 
